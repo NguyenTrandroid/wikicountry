810608d1481bcb022fe6c7904fe70c613136e243
nhat <trannhat2411999@gmail.com>
2018-08-19 01:06:31 +0700

    code info_country_activity

--- app/src/main/java/com/example/nguyentrandroid/wikicountry/Activity/Info_Country_Activity.java
+++ app/src/main/java/com/example/nguyentrandroid/wikicountry/Activity/Info_Country_Activity.java
@@ -2,14 +2,47 @@ package com.example.nguyentrandroid.wikicountry.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.nguyentrandroid.wikicountry.R;
 
 public class Info_Country_Activity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info__country_);
+        intit();
+        intdata();
+        intevent();
     }
+
+    private void intevent() {
+    }
+
+    private void intdata() {
+        setUpRecyclerView();
+        mAdapter = new AdapterInfoCountry;
+    }
+
+    private void intit() {
+        mRecyclerView = findViewById(R.id.actin_recycleview);
+
+    }
+
+    private void setUpRecyclerView() {
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
 }
