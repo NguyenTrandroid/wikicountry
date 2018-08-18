d9f194082172682cee7e301386bb1350e74e0961
NguyenTrandroid <nguyenmap2308@gmail.com>
2018-08-19 00:23:24 +0700

    Create Activity

---
+++ app/src/main/java/com/example/nguyentrandroid/wikicountry/Activity/List_Country_Activity.java
@@ -0,0 +1,15 @@
+package com.example.nguyentrandroid.wikicountry.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nguyentrandroid.wikicountry.R;
+
+public class List_Country_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list__country_);
+    }
+}
