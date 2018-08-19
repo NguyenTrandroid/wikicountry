1f5026a3db34a0ab40811023b9bbc845ae003872
nhat <trannhat2411999@gmail.com>
2018-08-20 01:11:07 +0700

    anh xa butter knife them searchview add gradle

--- app/src/main/java/com/example/nguyentrandroid/wikicountry/Activity/Info_Country_Activity.java
+++ app/src/main/java/com/example/nguyentrandroid/wikicountry/Activity/Info_Country_Activity.java
@@ -1,15 +1,48 @@
 package com.example.nguyentrandroid.wikicountry.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.nguyentrandroid.wikicountry.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Info_Country_Activity extends AppCompatActivity {
+
+    @BindView(R.id.imgFlag)
+    ImageView imgFlag;
+    @BindView(R.id.txtNameCt)
+    TextView txtNameCt;
+    @BindView(R.id.codeCT)
+    TextView codeCT;
+    @BindView(R.id.capitalCT)
+    TextView capitalCT;
+    @BindView(R.id.regionCT)
+    TextView regionCT;
+    @BindView(R.id.populationCT)
+    TextView populationCT;
+    @BindView(R.id.callingcodes)
+    TextView callingcodes;
+    @BindView(R.id.AreaCT)
+    TextView AreaCT;
+    @BindView(R.id.timezonesCT)
+    TextView timezonesCT;
+    @BindView(R.id.bordersCT)
+    TextView bordersCT;
+    @BindView(R.id.nativenameCT)
+    TextView nativenameCT;
+    @BindView(R.id.currenciesCT)
+    TextView currenciesCT;
+    @BindView(R.id.languagesCT)
+    TextView languagesCT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_country_);
+        ButterKnife.bind(this);
     }
 }
